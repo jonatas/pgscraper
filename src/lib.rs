39530fbbc21accd41f97589a04eb465b05eade4d
Jônatas Davi Paganini <jonatasdp@gmail.com>
2021-09-16 17:00:30 -0300

    rustfmt it!

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,10 @@ pg_module_magic!();
 
 #[pg_extern]
 fn http(url: &str) -> String {
-    let response = rttp_client::HttpClient::new().url(url).emit().expect("valid response");
+    let response = rttp_client::HttpClient::new()
+        .url(url)
+        .emit()
+        .expect("valid response");
     response.to_string()
 }
 
@@ -20,7 +23,7 @@ fn html_select_text(expression: &str, url: &str) -> String {
     let document = Html::parse_document(&http(&url));
     let selector = Selector::parse(expression).unwrap();
     let mut results = String::from("");
-    for element in document.select(&selector){
+    for element in document.select(&selector) {
         results.push_str(&element.inner_html());
         results.push_str("\n");
     }
